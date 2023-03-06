195ed8397f03d8bb9e7969dea43fc411be8663fb
oderagowski <oderagowski@gmail.com>
2023-03-06 13:35:13 -0700

    acme practice

---
+++ AcmeAccountingSystemsPractice/AcmeAccountingSystemsPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AcmeAccountingSystemsPractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Account Systems \r Remember, we're \" Accounting \" on you");
+            Console.ReadLine();
+        }
+    }
+}
