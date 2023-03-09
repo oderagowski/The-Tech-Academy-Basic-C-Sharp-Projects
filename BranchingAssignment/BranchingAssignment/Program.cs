25ecb824196c80fa73501557c46fe291c041280e
oderagowski <oderagowski@gmail.com>
2023-03-09 16:50:23 -0700

    fixed mistakes

--- BranchingAssignment/BranchingAssignment/Program.cs
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -19,28 +19,32 @@ namespace BranchingAssignment
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
 
-            
+            else
             {
                 Console.WriteLine("Please enter the package width: ");
-                
+                // converts user input to integer
+                int packageWidth = int.Parse(Console.ReadLine());
+
+
+
+                Console.WriteLine("Please enter the package width: ");
                 // converts user input to integer
                 int packageHeight = int.Parse(Console.ReadLine());
 
 
 
-                Console.WriteLine("Please enter the package length: ");
-                
+                Console.WriteLine("Please enter the package length: "); 
                 // converts user input to integer
                 int packageLength = int.Parse(Console.ReadLine());
 
                 // program will end if combined height and length is greater than 50
-                if (packageHeight + packageLength > 50)
+                if (packageWidth + packageHeight + packageLength > 50)
                 {
                     Console.WriteLine("Package too big to be shipped via Package Express");
                 }
                 else // will run if combined height and length is less than 50
                 {
-                    int priceQuote = (packageHeight * packageLength * packageWeight) / 100;
+                    int priceQuote = ((packageHeight * packageWidth * packageLength) * packageWeight) / 100;
 
                     Console.WriteLine("Your estimated total for shipping this package is: $" + priceQuote + "\nThank you! ");
                 }
