511f0bd5e3ab0d19fde1c3cecba31f4985b45ff2
oderagowski <oderagowski@gmail.com>
2023-03-20 16:37:16 -0600

    Method assignment submission

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/MathOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class MathOperation
+    {
+        // setting default value of second parameter to 0 in order to make it optional
+        public static int DoMath(int number1, int number2 = 0)
+        {
+            return (number1 + number2) * 100;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates the class
+            
+            MathOperation mathOperation = new MathOperation();
+            
+            
+            // asks user to input two numbers one at a time
+            Console.WriteLine("Enter the first number: ");
+            int number1 = int.Parse(Console.ReadLine());
+
+            // user can skip number, which will cause the "if" statement to execute
+            Console.WriteLine("Enter the second number (optional): ");
+            string optionalNumber = Console.ReadLine();
+
+            if (optionalNumber == "")
+            {
+                Console.WriteLine($"{number1} multiplied by 100 equals: " + MathOperation.DoMath(number1));
+            }
+            
+            // executes if the user inputs two numbers
+            else
+            {
+                int number2 = Convert.ToInt32(optionalNumber);
+                Console.WriteLine($"{number1} plus {number2} multiplied by 100 equals: " + MathOperation.DoMath(number1, number2));
+            }
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+}
