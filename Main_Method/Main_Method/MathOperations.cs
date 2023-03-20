401b4e579837ee8185659199af9cc9c45606bef1
oderagowski <oderagowski@gmail.com>
2023-03-20 11:11:14 -0600

    Main Method assignment submission

---
+++ Main_Method/Main_Method/MathOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class MathOperations
+    {
+        // takes in an integer and performs a math operation
+        public static int DoMath(int number)
+        {
+            return number * 10;
+        }
+
+        // takes in a decimal, converts decimal to integer, and performs a math operation
+        public static int DoMath(decimal number)
+        {
+            int newNumber = Convert.ToInt32(number);;
+            return newNumber * 100;
+
+        }
+
+        // takes in a string, converts string to integer, and performs a math operation
+        public static int DoMath(string number)
+        {
+            int newNumber = Int32.Parse(number);
+            return newNumber + 1500;
+        }
+    }
+}
---
+++ Main_Method/Main_Method/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // instantiating class
+            MathOperations mathOperations = new MathOperations();
+
+            // numbers that will be passed into methods
+            int number1 = 10;
+            decimal number2 = 44.5M;
+            string number3 = "1000";
+
+            // calling first method, passing in an integer
+            int result1 = MathOperations.DoMath(number1);
+            Console.WriteLine(result1);
+
+            // calling second method, passing in a decimal
+            int result2 = MathOperations.DoMath(number2);
+            Console.WriteLine(result2);
+
+            // calling third method, passing in a string
+            int result3 = MathOperations.DoMath(number3);
+            Console.WriteLine(result3);
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
