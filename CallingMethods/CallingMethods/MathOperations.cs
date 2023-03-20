e430ac174718853c02d3608f40845273d113d5c6
oderagowski <oderagowski@gmail.com>
2023-03-20 09:32:20 -0600

    Calling Methods assignment submission

---
+++ CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    
+    // Create a class
+    public class MathOperations
+    {
+        // takes an integer multiplies it by 2
+        public static int MultiplyByTwo(int number)
+        {
+            return number * 2;
+        }
+
+        // takes an integer and multiplies it by itself
+        public static int NumberSquared (int number)
+        {
+            return number * number;
+        }
+
+        // takes and integer and returns either 0 or 1
+        public static int EvenOrOdd (int number)
+        {
+            return number % 2;
+        }
+
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asks the user what number they want to do the math operations on
+            Console.WriteLine("Enter a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            
+            // calling method 1
+            int NumTimesTwo = MathOperations.MultiplyByTwo(userNumber);
+            Console.WriteLine($"{userNumber} multiplied by two is {NumTimesTwo}");
+
+            // calling method 2
+            int NumSquared = MathOperations.NumberSquared(userNumber);
+            Console.WriteLine($"{userNumber} squared is {NumSquared}");
+
+            // calling method 3
+            int NumCheck = MathOperations.EvenOrOdd(userNumber);
+            if (NumCheck == 0)
+            {
+                Console.WriteLine($"{userNumber} is an even number");
+            }
+            else
+            {
+                Console.WriteLine($"{userNumber} is an odd number");
+            }
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
