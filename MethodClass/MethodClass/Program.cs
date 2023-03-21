ed941a227e1a0862d2f9cbf01f6f83c41108e349
oderagowski <oderagowski@gmail.com>
2023-03-21 07:25:45 -0600

    Method class assignment submission

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating the class
+            MathOperation mathOperation = new MathOperation();
+
+            // passing in two numbers
+            mathOperation.DoMath(10, 20);
+
+            // specifying the parameters by name
+            mathOperation.DoMath(number1: 5, number2: 25);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClass/MethodClass/VoidMathOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class MathOperation
+    {
+        
+        // void method that takes two integers as parameters and returns nothing
+        public void DoMath(int number1, int number2)
+        {
+            // math operation performed on the first integer
+            int squared = number1 * number1;
+
+            // displays the second integer to the screen
+            Console.WriteLine($"The second number is {number2}");
+        }
+    }
+}
