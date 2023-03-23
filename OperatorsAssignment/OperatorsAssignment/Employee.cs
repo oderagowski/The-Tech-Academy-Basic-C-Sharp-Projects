a2b9c455f9cf39dfcf7cff67150111b03c43f364
oderagowski <oderagowski@gmail.com>
2023-03-23 08:02:58 -0600

    Operator assignment submission

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    // creating an Employee class with ID, FirstName, and LastName properties
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        // overloading "==" operator; compares employee ID properties and returns true or false
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.ID == employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // operators must be overloaded in pairs
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1.ID == employee2.ID);
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating two objects and assigning values to their properties
+            Employee employee1 = new Employee { ID = 4480, FirstName = "Bruce", LastName = "Wayne" };
+
+            Employee employee2 = new Employee { ID = 4580, FirstName = "Peter", LastName = "Parker" };
+
+
+            // comparing two Employee objects using overloaded operator and displaying results
+            Console.Write($"Do the employees have the same ID?: {employee1 == employee2} \n");
+
+            Console.Write($"Do these two employees have different IDs?: {employee1 != employee2}");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
