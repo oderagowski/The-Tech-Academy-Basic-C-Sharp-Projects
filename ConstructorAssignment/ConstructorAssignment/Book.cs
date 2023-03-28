af1698f4518311d96e305eaf6f675ed82a5c3fc1
oderagowski <oderagowski@gmail.com>
2023-03-28 07:02:22 -0600

    Constructor assignment submission

---
+++ ConstructorAssignment/ConstructorAssignment/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Book
+    {
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public int CopiesInStock { get; set; }
+
+        // chain two constructors together
+        public Book(string title, string author) : this(title, author, 0)
+        {
+
+        }
+
+        public Book(string title, string author, int copiesInStock)
+        {
+            Title = title;
+            Author = author;
+            CopiesInStock = copiesInStock;
+
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a const variable
+            const string storeName = "Barnes and Noble";
+
+            // creating variable using keyword "var"
+            var shelfK = new List<Book>();
+
+            
+            // initializing objects to make sure code works properly
+            Book book1 = new Book("It", "King, Stephen", 100);
+            Book book2 = new Book("Demon Copperhead", "Kingsolver, Barbara", 55);
+            Book book3 = new Book("A Separate Peace", "Knowles, John");
+
+
+            shelfK.Add(book1);
+            shelfK.Add(book2);
+            shelfK.Add(book3);
+
+            Console.WriteLine("On Shelf K: ");
+            foreach (Book book in shelfK)
+            {
+                Console.WriteLine($"There are {book.CopiesInStock} copies of {book.Title} by {book.Author}");
+            }
+
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
